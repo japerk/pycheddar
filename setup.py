9a398674b31ad76ee7d87e42fd499934be95f6f4
Ryan Showalter <ryan@newton.ryants.com>
2010-07-15 16:35:42 -0700

    re-configure package layout to accommodate pip's requirements.txt

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+setup(
+    name='pycheddar',
+    version='0.9.3',
+    description='A Python wrapper for CheddarGetter.',
+    author='FeedMagnet',
+    url='http://www.feedmagnet.com/blog/cheddargetter-for-python-and-django/',
+    requires=['httplib2',],
+    packages=['pycheddar',],
+    package_dir={'pycheddar': 'src'},
+)
