04b229047b6d5254f87d5e03c01fca70bc13d749
Ryan Showalter <ryan@newton.ryants.com>
2010-08-11 13:03:50 -0700

    Change the version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from distutils.core import setup
 
 setup(
     name='pycheddar',
-    version='0.9.3',
+    version='0.9.3-ryanshow1',
     description='A Python wrapper for CheddarGetter.',
     author='FeedMagnet',
     url='http://www.feedmagnet.com/blog/cheddargetter-for-python-and-django/',
